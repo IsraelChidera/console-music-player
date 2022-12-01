3b1abb0f8ece5629bf00fad37b6b5ee0e47357f3
IsraelChidera <israelchidera54@gmail.com>
2022-12-01 07:23:42 +0100

    Add project

--- MusicPlayer/Songs/SongList.cs
+++ MusicPlayer/Songs/SongList.cs
@@ -1,4 +1,6 @@
-﻿namespace MusicPlayer.Songs
+﻿using MusicPlayer.Designs.Utility;
+
+namespace MusicPlayer.Songs
 {
     class SongCollection
     {
@@ -31,13 +33,62 @@
             songCollection.Add(new SongCollection() { Artist = "Fruyi", SongName = "Friends" });
             songCollection.Add(new SongCollection() { Artist = "Tonali", SongName = "Grand dad's love" });
             songCollection.Add(new SongCollection() { Artist = "Folu", SongName = "Go get 'em" });
-
+            //songCollection.Sort();
 
             Console.WriteLine("All songs\n- \tArtist -- \tSong Title");
 
-            foreach (var song in songCollection)
+            var results = songCollection
+                      .OrderByDescending(y => y.Artist);
+                      
+
+            foreach (var song in results)
             {
                 Console.WriteLine($"- \t{song.Artist} -- \t{song.SongName}");
+            }
+
+            while (true)
+            {
+                Utility.LongLine();
+                Console.WriteLine("\tTo play all songs, press 1\n\tTo play previous song, press 2\n\tTo next song" +
+                    " press 3\n\tTo shuffle song, press 4\n\tTo add song, press 5\n\tTo exit, press 0\n");
+                
+                Console.Write("====>");
+                string musicOption = Console.ReadLine();
+
+                Console.WriteLine(musicOption);
+
+                try
+                {
+                    switch (musicOption)
+                    {
+                        case "1":
+                            Console.WriteLine("Playing all songs");
+                            foreach (var song in results)
+                            {
+                                Console.WriteLine($"Now playing\n- \t-- \t{song.SongName}");
+                                Thread.Sleep(2000);
+                            }
+                            break;                        
+                        case "2":
+                            Console.WriteLine("Play previous song");
+                            break;
+                        case "3":
+                            Console.WriteLine("Play next song");
+                            break;
+                        case "4":
+                            Console.WriteLine("Shuffle");
+                            break;
+                        case "5":
+                            Console.WriteLine("Adding songs");
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException("Invalid input");
+                    }
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
             }
 
 
